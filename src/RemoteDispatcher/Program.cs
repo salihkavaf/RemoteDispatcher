d6297ee11180f0bc7556c0695f80b77158dc0ebf
Salih Kavaf <52260488+salihkavaf@users.noreply.github.com>
2020-11-08 18:18:29 +0300

    Setting up the tool basics.

---
+++ src/RemoteDispatcher/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Reflection;
+
+namespace RemoteDispatcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                var versionString =
+                    Assembly.GetEntryAssembly()
+                        .GetCustomAttribute<AssemblyInformationalVersionAttribute>()
+                        .InformationalVersion
+                        .ToString();
+
+                Console.WriteLine($"salih-rd v{versionString}");
+                Console.WriteLine("-------------");
+                Console.WriteLine("\nUsage:");
+                Console.WriteLine("  salih-rd <message>");
+                return;
+            }
+        }
+    }
+}
